0fbfc89579e89a2358c58f01ad7a950c08e987a6
Niden <80420845+Niden1@users.noreply.github.com>
2021-11-27 10:40:18 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const rateLimit = require("express-rate-limit")
+const PORT = process.env.PORT || 8000
+
+
+// Rate limiter
+const limiter = rateLimit({
+    windowMs: 100 * 60 * 100, // 10 minutes
+    max: 100, // 100 requests max every 10 minutes
+    message: "You are sending too many requests. Please try again later."
+})
+
+app.get('/hello', (req, res) => {
+
+    res.status(200).send({
+        url: "Hello World!"
+    });
+});
+
+// Listen on port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+    console.log('API Online');
+});
